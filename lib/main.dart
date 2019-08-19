0e2dadf85df36fdc25f0bb1d4c7351771ab25576
Chirag <chiragsaraogi10k@gmail.com>
2019-08-19 23:59:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_walkthrough/flutter_walkthrough.dart';
+import 'package:flutter_walkthrough/walkthrough.dart';
+
+void main() => runApp(new MaterialApp(
+  home: new Scaffold(
+    body: new TestScreen(),
+  )
+  ));
+
+class TestScreen extends StatelessWidget {
+  final List<Walkthrough> list = [
+    Walkthrough(
+      title: "Title 1",
+      content: "Content 1",
+      imageIcon: Icons.restaurant_menu,
+    ),
+    Walkthrough(
+      title: "Title 2",
+      content: "Content 2",
+      imageIcon: Icons.search,
+    ),
+    Walkthrough(
+      title: "Title 3",
+      content: "Content 3",
+      imageIcon: Icons.shopping_cart,
+    ),
+    Walkthrough(
+      title: "Title 4",
+      content: "Content 4",
+      imageIcon: Icons.verified_user,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new IntroScreen(
+      list,
+      new MaterialPageRoute(builder: (context) => new TestScreen()),
+    );
+  }
+}
